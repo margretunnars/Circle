3aa71f2f8a211ca3c2c37f523bc061892c12b19b
Margrét Unnarsdóttir <margretunnarsdottir@Margrets-MacBook-Pro.local>
2016-10-17 11:52:45 +0100

    first commit

---
+++ Circle/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Circle
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double radius;
+			string input;
+			double pi = 3.14149;
+
+
+			Console.WriteLine(" What is the radius of the circle?");
+			input = Console.ReadLine();
+			radius = double.Parse(input);
+
+			double diameter = 2 * radius;
+			double circumference = 2 * pi * radius;
+			double area = pi * radius * radius;
+
+			Console.WriteLine("The diameter of the circle is " + diameter);
+			Console.WriteLine("The circumference of the circle is " + circumference);
+			Console.WriteLine("The area of the circle is " + area);
+
+		}
+	}
+}
